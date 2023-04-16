375492a72b69b6d3c4761161b24e32a1cf9f2992
Ozmar Mendoza <amfc11@gmail.com>
2023-04-16 11:33:53 -0500

    Set up PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,27 +1,29 @@
-// If using CSS modules:
-// https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-custom-media#modular-css-processing
-
 const prod = process.env.NODE_ENV === "production";
 
-export const plugins = {
-	"postcss-preset-env": {
-		autoprefixer: prod ? true : false,
-		stage: 2,
-		features: {
-			"nesting-rules": true,
-			"custom-media-queries": true,
-			"all-property": false,
-			"any-link-pseudo-class": false,
-			"custom-properties": false,
-			"font-variant-property": false,
-			"gap-properties": prod ? true : false,
-			"image-set-function": false,
-			"is-pseudo-class": false,
-			"logical-properties-and-values": false,
-			"not-pseudo-class": false,
-			"overflow-property": false,
-			"overflow-wrap-property": false,
-			"unset-value": false,
+module.exports = {
+	plugins: {
+		"@csstools/postcss-global-data": {
+			files: ["src/assets/styles/custom-media-queries.css"],
+		},
+		"postcss-preset-env": {
+			autoprefixer: prod ? true : false,
+			stage: 2,
+			features: {
+				"nesting-rules": true,
+				"custom-media-queries": true,
+				"all-property": false,
+				"any-link-pseudo-class": false,
+				"custom-properties": false,
+				"font-variant-property": false,
+				"gap-properties": prod ? true : false,
+				"image-set-function": false,
+				"is-pseudo-class": false,
+				"logical-properties-and-values": false,
+				"not-pseudo-class": false,
+				"overflow-property": false,
+				"overflow-wrap-property": false,
+				"unset-value": false,
+			},
 		},
 	},
 };
