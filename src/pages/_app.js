ea2572d99371e06f2b89a669b7a4958e975c3267
Ozmar Mendoza <amfc11@gmail.com>
2023-05-21 23:40:06 -0500

    Set up Supabase helpers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,18 @@
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+import { useState } from "react";
+
 import "@styles/global.css";
 
 export default function ({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	const [supabaseClient] = useState(() => createBrowserSupabaseClient());
+	return (
+		<SessionContextProvider
+			supabaseClient={supabaseClient}
+			initialSession={pageProps.initialSession}
+		>
+			<Component {...pageProps} />
+		</SessionContextProvider>
+	);
 }
