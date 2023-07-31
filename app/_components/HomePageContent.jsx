8d85e472299aaf40a8b82fe29312aaaddb8f37c4
Ozmar Mendoza <amfc11@gmail.com>
2023-07-30 23:20:11 -0500

    Minor styling updates

--- app/_components/HomePageContent.jsx
+++ app/_components/HomePageContent.jsx
@@ -1,6 +1,6 @@
 export default function HomePageContent({ styles }) {
 	return (
-		<div className="section-container">
+		<div className={`section-container ${styles.contentContainer}`}>
 			<h1 className={styles.heading}>Real Time Video Commentary</h1>
 			<h2 className={styles.subheading}>
 				Make movie binging a little more interesting
